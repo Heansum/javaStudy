aeaf93577a012fccbf29bbfd2a577127eafd81e5
DESKTOP-NUOPUIF\ITPS <heansum1556@gmail.com>
2021-03-15 17:19:06 +0900

    최초 프로젝트 업로드

---
+++ src/chapter1/Ex2.java
@@ -0,0 +1,29 @@
+package chapter1;
+
+public class Ex2 {
+	public static void main(String[] args) {
+		// 문자열 - 문자의 나열
+		// 문자열은 꼭 쌍따옴표를 사용해서 표현해야한다.
+//		System.out.println("Hello World~!");
+//		System.out.println();
+//		System.out.println("Hello");
+//		System.out.println("World~!");
+		
+//		System.out.print("I'm ");
+//		System.out.print("a ");
+//		System.out.print("Java Programmer");
+		
+//		System.out.print("오늘은 ");
+//		System.out.print("자바 수업 ");
+//		System.out.print("첫 번째 날입니다.");
+		
+		//정수
+		System.out.println(1);
+		//실수
+		System.out.println(3.14);
+		//문자
+		System.out.println('a');
+		//문자열
+		System.out.println("abcd");
+	}
+}
---
+++ src/chapter1/Ex3.java
@@ -0,0 +1,19 @@
+package chapter1;
+
+public class Ex3 {
+	public static void main(String[] args) {
+//		System.out.print("오늘의 기온 : ");
+//		System.out.println(-5.3);
+//		System.out.println("");
+//		
+//		System.out.print("커피 가격: ");
+//		System.out.println(1500);
+//		
+//		System.out.print("철수의 Java 과목 학점 : ");
+//		System.out.println('A');
+		
+		System.out.println("오늘의 기온 : "+ -5.3 + "");
+		System.out.println("커피 가격: " + 1500);
+		System.out.println("철수의 Java 과목 학점 : " + 'A');
+	}
+}
